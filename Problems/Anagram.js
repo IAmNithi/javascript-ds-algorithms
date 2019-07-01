350f50e53a30d06a687b5d9066b71d730074eff2
IAmNithi <kalanithi_sas@outlook.com>
2019-07-01 13:36:14 +0530

    adding anagram problem

---
+++ Problems/Anagram.js
@@ -0,0 +1,29 @@
+// Given two strings, write a function to determine is the first string is anagram of the second. 
+// An anagram is a word, phase, or name formated by rearranging the letters of another, such as cinema, from iceman.  
+
+function validAnagram(a,b){
+    if(a.length !== b.length){
+        console.log(false);
+    }
+    let lookup = {};
+    for(let i=0; i<a.length; i++){
+        let letter = a[i];
+        lookup[letter] ? lookup[letter] +=  1 : lookup[letter] = 1;
+    }
+    for(let i=0; i<b.length;i++){
+        let letter = b[i];
+        if(!(lookup[letter])){
+            console.log(false);
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    console.log(true);
+}
+
+validAnagram('', '');
+validAnagram('aaz', 'zza');
+validAnagram('rat', 'car');
+validAnagram('anagram', 'nagaram');
+validAnagram('awesome', 'awesom');
+validAnagram('texttwisttime', 'timetwisttext');
