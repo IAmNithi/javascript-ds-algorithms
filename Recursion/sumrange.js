5531a02d452043b6e9f67b53f551a632e7ecc298
IAmNithi <kalanithi_sas@outlook.com>
2019-07-03 15:33:07 +0530

    added 2 more examples

---
+++ Recursion/sumrange.js
@@ -0,0 +1,10 @@
+function sumRange(num){
+   if(num === 1) return 1; 
+   return num + sumRange(num-1);
+}
+
+sumRange(4)
+
+                  
+                                   
+                                
