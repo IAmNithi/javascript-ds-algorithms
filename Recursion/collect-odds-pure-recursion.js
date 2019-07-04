654cf6361f0e6ae8e5f63d7a600c3c8b8547ed0d
IAmNithi <kalanithi_sas@outlook.com>
2019-07-04 15:33:18 +0530

    added pure recursion function

---
+++ Recursion/collect-odds-pure-recursion.js
@@ -0,0 +1,21 @@
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+collectOddValues([1,2,3,4,5])
+                                        
+                                                                
+                                                                             
+                            
+            
