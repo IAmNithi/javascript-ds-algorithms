21033e7a44cb8082cf5c54eff81789b7ad2e7dfc
IAmNithi <kalanithi_sas@outlook.com>
2019-07-08 13:48:51 +0530

    added naive string search problem

---
+++ Searching Algorithms/Naive String Search/string-search.js
@@ -0,0 +1,12 @@
+function naiveSearch(long, short){
+    var count = 0;
+    for(var i = 0; i < long.length; i++){
+        for(var j = 0; j < short.length; j++){
+           if(short[j] !== long[i+j]) break;
+           if(j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
+
+naiveSearch("lorie loled", "lol")
