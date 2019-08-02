77b624a86244548fd4c6c815956744e2892d877a
IAmNithi <kalanithi_sas@outlook.com>
2019-08-02 16:17:02 +0530

    added new problem

---
+++ Problems/findPair.js
@@ -0,0 +1,24 @@
+// array of integers
+// find the pair 
+// condition --> same pair Should not return
+
+function findPair(arr, pair){
+    let result = {};
+    for(var i =0; i< arr.length;i++){
+      for(var j=i+1;j<arr.length;j++){
+        if(arr[i]+arr[j] == pair){
+          var ar = []; 
+          ar.push(arr[i]);
+          ar.push(arr[j]);
+          result[i] ? result[i] = result[i] : result[i] = ar;
+        }
+      }
+    }
+    console.log(result);
+  }
+  
+  var a = [0,5,10,15,20,25,30];
+  var b = 25;
+  
+  findPair(a,b);
+  
