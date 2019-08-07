7c2923a7ea153551de37de4a2d9e5ee4e4d6e933
IAmNithi <kalanithi_sas@outlook.com>
2019-08-07 12:08:56 +0530

    added basic hash js

---
+++ Data Structure/Hash Table/basic-hash.js
@@ -0,0 +1,9 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  for (let char of key) {
+    // map "a" to 1, "b" to 2, "c" to 3, etc.
+    let value = char.charCodeAt(0) - 96
+    total = (total + value) % arrayLen;
+  }
+  return total;
+}
