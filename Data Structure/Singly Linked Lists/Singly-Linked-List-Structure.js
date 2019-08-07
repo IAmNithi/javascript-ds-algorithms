2e1ad4cfb334fa3ffc4aeb3cb5de1fa543ee175a
IAmNithi <kalanithi_sas@outlook.com>
2019-08-08 00:47:17 +0530

    added basic version

---
+++ Data Structure/Singly Linked Lists/Singly-Linked-List-Structure.js
@@ -0,0 +1,32 @@
+// piece of data - val
+//reference to next node - next
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        
+    }
+}
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+
+
