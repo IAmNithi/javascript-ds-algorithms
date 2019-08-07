fe452b3979fc6e0ef65a6d2c8e8246f9607e17b7
IAmNithi <kalanithi_sas@outlook.com>
2019-08-07 12:11:57 +0530

    added improved hash function

---
+++ Data Structure/Hash Table/improved-hash.js
@@ -0,0 +1,10 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
