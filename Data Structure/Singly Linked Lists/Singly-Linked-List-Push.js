ff26f9306e3678d9c1723952e71f91059a9e7c2d
IAmNithi <kalanithi_sas@outlook.com>
2019-08-10 15:59:50 +0530

    added singly linked list push js

---
+++ Data Structure/Singly Linked Lists/Singly-Linked-List-Push.js
@@ -0,0 +1,30 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+var list = new SinglyLinkedList()
+// list.push("HELLO")
+// list.push("GOODBYE")
