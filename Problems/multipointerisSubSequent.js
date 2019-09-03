227278406fe7bebcc3629e55ad9ae30c297c9b19
IAmNithi <kalanithi_sas@outlook.com>
2019-09-03 15:11:07 +0530

    added multipointer subsequent problem

---
+++ Problems/multipointerisSubSequent.js
@@ -0,0 +1,13 @@
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+function isSubsequence(str1, str2) {
+    if(str1.length === 0) return true
+    if(str2.length === 0) return false
+    if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+    return isSubsequence(str1, str2.slice(1))
+  }
+
+isSubsequence('hello','hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false
