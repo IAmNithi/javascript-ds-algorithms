394b299f700d0a2cac1d8ae0f1afd366d0e29604
IAmNithi <kalanithi_sas@outlook.com>
2019-09-08 18:36:25 +0530

    added solution

---
+++ Recursion/nestedEvenSum.js
@@ -0,0 +1,11 @@
+// Write a recursive function called nestedEvenSum. Return the sum of all even numbers in an object which may contain nested objects.
+function nestedEvenSum (obj, sum=0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
