63dc8326170162ca298ba1e6e02989659b4a00f6
IAmNithi <kalanithi_sas@outlook.com>
2019-09-08 18:37:38 +0530

    added nestedEvenSum test case

--- Recursion/nestedEvenSum.js
+++ Recursion/nestedEvenSum.js
@@ -9,3 +9,26 @@ function nestedEvenSum (obj, sum=0) {
     }
     return sum;
 }
+
+var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+  
+  var obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+  };
+  
+  nestedEvenSum(obj1); // 6
+  nestedEvenSum(obj2); // 10
