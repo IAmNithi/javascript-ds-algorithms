6ddaa447483a4ab6769a64aa583fe389b50b5ed9
IAmNithi <kalanithi_sas@outlook.com>
2019-09-12 19:22:25 +0530

    problem solution

---
+++ Recursion/collectStrings.js
@@ -0,0 +1,13 @@
+function collectStrings(obj) {
+    var stringsArr = [];
+    for(var key in obj) {
+        if(typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        }
+        else if(typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+ 
+    return stringsArr;
+}
