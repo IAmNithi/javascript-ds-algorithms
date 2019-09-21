600e5b88ef082df34662a47ab02a0c61fccd4830
IAmNithi <kalanithi_sas@outlook.com>
2019-09-21 20:40:24 +0530

    radix sort helper

---
+++ Sorting/Radix Sort/radix-helpers.js
@@ -0,0 +1,28 @@
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+mostDigits([23,567,89,12234324,90])
+
+
+
+
+
+
+
+
+
+
